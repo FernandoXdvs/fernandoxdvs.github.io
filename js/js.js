d70df7e78b45962dc6e8e68971cc0b6f3f74244f
FernandoXdvs <144376304+FernandoXdvs@users.noreply.github.com>
2024-03-19 12:38:33 -0600

    Añadir js

---
+++ js/js.js
@@ -0,0 +1,7 @@
+let continuaAbajo= document.querySelector(".text-img");
+
+window.onload=()=>{
+    continuaAbajo.addEventListener("click", ()=>{
+        continuaAbajo.style="display:none";
+    });
+}
